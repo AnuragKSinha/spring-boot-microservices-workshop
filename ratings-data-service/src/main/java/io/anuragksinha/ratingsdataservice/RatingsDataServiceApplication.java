d49b7af7e7469928994af92c39978ba14296f860
Anurag Sinha <sinhaanurag18@gmail.com>
2023-04-02 00:07:37 +0530

    service changes

---
+++ ratings-data-service/src/main/java/io/anuragksinha/ratingsdataservice/RatingsDataServiceApplication.java
@@ -0,0 +1,13 @@
+package io.anuragksinha.ratingsdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsDataServiceApplication.class, args);
+	}
+
+}
--- ratings-data-service/src/main/java/io/anuragksinha/ratingsdataservice/resources/RatingResource.java
+++ ratings-data-service/src/main/java/io/anuragksinha/ratingsdataservice/resources/RatingResource.java
@@ -1,2 +1,16 @@
-package io.anuragksinha.ratingsdataservice.resources;public class RatingResource {
+package io.anuragksinha.ratingsdataservice.resources;
+
+import io.anuragksinha.ratingsdataservice.models.Rating;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingResource {
+	@RequestMapping("/{movieId}")
+	public Rating getRating(@PathVariable("movieId") String movieId){
+		return new Rating(movieId,4);
+	}
 }
