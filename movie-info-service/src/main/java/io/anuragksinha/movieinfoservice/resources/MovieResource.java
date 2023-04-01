291e0b7940663cef12ecc691e45840da0e7b3479
Anurag Sinha <sinhaanurag18@gmail.com>
2023-04-02 00:07:07 +0530

    first set

--- movie-info-service/src/main/java/io/anuragksinha/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/io/anuragksinha/movieinfoservice/resources/MovieResource.java
@@ -1,2 +1,16 @@
-package io.anuragksinha.movieinfoservice.resources;public class MovieResource {
+package io.anuragksinha.movieinfoservice.resources;
+
+import io.anuragksinha.movieinfoservice.models.Movie;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+	@RequestMapping("/{movieId}")
+	public Movie getMovieInfo(@PathVariable("movieId") String movieId){
+		return new Movie("998","Transformer");
+	}
 }
